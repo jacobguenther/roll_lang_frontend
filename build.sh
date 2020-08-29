1712ee6608fbb51912cae20df9a818935f4bc222
jacobguenther <jacobrwguenther@protonmail.com>
2020-08-28 22:06:58 -0800

    Change location of files.

--- build.sh
+++ build.sh
@@ -2,30 +2,31 @@
 
 
 # Copyright (C) 2020  Jacob Guenther
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 echo "Building roll lang frontend"
 wasm-pack build --target no-modules --out-dir temp
 
-mv temp/roll_lang_frontend* dist/scripts/
 
-cp -r www/assets/*  dist/assets/
-cp -r www/css/*     dist/css/
-cp -r www/html/*    dist/pages/
-cp -r www/js/* dist/scripts/
+mkdir --parents dist/static/assets
+mkdir --parents dist/static/html
+mkdir --parents dist/static/css
+mkdir --parents dist/static/scripts
+mkdir --parents dist/templates
 
-cp www/javascript.html dist/about/javascript.html
-cp www/index.html   dist/index.html
+cp temp/roll_lang_frontend* dist/static/scripts/
 
+cp -r www/static/*    dist/static/
+cp -r www/templates/* dist/templates/
